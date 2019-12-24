320783347392fc53749badf640faeb6879c2e3d7
babel <babel2008@gmail.com>
2019-12-24 11:07:44 +0100

    Drobne zmiany

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,14 +1,16 @@
-const {Task, validate} = require('../models/task'); 
+const {Task, validate} = require('../models/task');
+const  auth = require('../middleware/auth'); 
+const {User} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
   const tasks = await Task.find().sort('status');
   res.send(tasks);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -16,7 +18,10 @@ router.post('/', async (req, res) => {
   const task = new Task({ 
     content: req.body.content,
     status: req.body.status,
-    userId: req.body.userId
+    user: {
+      _id: user._id,
+      name: user.name
+    }
     
   });
   await task.save();
@@ -24,7 +29,7 @@ router.post('/', async (req, res) => {
   res.send(task);
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', auth, async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -46,7 +51,7 @@ router.put('/:id', async (req, res) => {
 });
 
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', auth, async (req, res) => {
   const task = await Task.findById(req.params.id);
 
   if (!task) return res.status(404).send('The task with the given ID was not found.');
